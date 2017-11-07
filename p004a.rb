8fab717b5417a1b577b4fac4138dfca8c1b69a7f
Dan Cecile <dancecile@gmail.com>
2017-11-07 11:56:55 -0500

    Switch Problem 4's test runner, and apply Rubocop

--- p004a.rb
+++ p004a.rb
@@ -1,92 +1,32 @@
 # Largest palindrome product
 # https://projecteuler.net/problem=4
 
-require "minitest/autorun"
-
+# Directly generate all numbers with correct number of digits,
+# cross-multiple all, then find the largest palindrome
 class Solution
-  def is_palindrome?(n)
+  def self.palindrome?(n)
     text = n.to_s
     text == text.reverse
   end
 
-  def generate_source(digits)
+  def self.generate_source(digits)
     from = digits == 1 ? 0 : 10**(digits - 1)
     to = 10**digits - 1
     (from..to).to_a
   end
 
-  def create_products(source)
-    source.repeated_combination(2)
+  def self.create_products(source)
+    source
+      .repeated_combination(2)
       .map do |pair|
         pair[0] * pair[1]
       end
   end
 
-  def find_solution()
+  def self.find_solution
     create_products(generate_source(3))
-      .select(&method(:is_palindrome?))
+      .select(&method(:palindrome?))
       .sort
       .last
-  end
-end
-
-describe Solution do
-  before do
-    @solution = Solution.new
-  end
-
-  describe "when checking for palidromes" do
-    it "must work with even lengths" do
-      @solution.is_palindrome?(88).must_equal true
-      @solution.is_palindrome?(80).must_equal false
-      @solution.is_palindrome?(8008).must_equal true
-      @solution.is_palindrome?(3920).must_equal false
-    end
-    it "must work with odd lengths" do
-      @solution.is_palindrome?(2).must_equal true
-      @solution.is_palindrome?(232).must_equal true
-      @solution.is_palindrome?(52032).must_equal false
-    end
-  end
-
-  describe "when creating source input" do
-    it "must be in range" do
-      @solution.generate_source(1).each do |source|
-        source.must_be :>=, 0
-        source.must_be :<=, 9
-      end
-      @solution.generate_source(2).each do |source|
-        source.must_be :>=, 10
-        source.must_be :<=, 99
-      end
-    end
-    it "must be correct length" do
-      @solution.generate_source(1).length.must_equal 10
-      @solution.generate_source(2).length.must_equal 90
-    end
-  end
-
-  describe "when creating products" do
-    it "must contain all entries" do
-      @solution.create_products([1, 2, 3]).must_equal [
-        1 * 1,
-        1 * 2,
-        1 * 3,
-        2 * 2,
-        2 * 3,
-        3 * 3,
-      ]
-      @solution.create_products([91, 99]).must_equal [
-        91 * 91,
-        91 * 99,
-        99 * 99,
-      ]
-    end
-  end
-
-  describe "when finding the solution" do
-    it "must output the answer" do
-      puts @solution.find_solution
-    end
   end
 end
---
+++ p004a_test.rb
@@ -0,0 +1,64 @@
+require "p004a"
+
+RSpec.describe Solution do
+  describe "::palindrome?" do
+    test_cases = {
+      88 => true,
+      80 => false,
+      8_008 => true,
+      3_920 => false,
+      2 => true,
+      232 => true,
+      52_032 => false
+    }
+    test_cases.each do |input, output|
+      it "checks #{input}" do
+        expect(Solution.palindrome?(input)).to be output
+      end
+    end
+  end
+
+  describe "::generate_source" do
+    test_cases = [
+      [1, 0, 9],
+      [2, 10, 99],
+      [3, 100, 999]
+    ]
+    test_cases.each do |input, from, to|
+      it "has correct length and range for #{input}" do
+        expect(Solution.generate_source(input)).to contain_exactly(
+          *[a_value_between(from, to)] * (to - from + 1)
+        )
+      end
+    end
+  end
+
+  describe "::create_products" do
+    test_cases = {
+      [1, 2, 3] => [
+        1 * 1,
+        1 * 2,
+        1 * 3,
+        2 * 2,
+        2 * 3,
+        3 * 3
+      ],
+      [91, 99] => [
+        91 * 91,
+        91 * 99,
+        99 * 99
+      ]
+    }
+    test_cases.each do |input, output|
+      it "multiplies all entries" do
+        expect(Solution.create_products(input)).to eq(output)
+      end
+    end
+  end
+
+  describe "when finding the solution" do
+    it "must output the answer" do
+      expect(Solution.find_solution).to eq(906_609)
+    end
+  end
+end
